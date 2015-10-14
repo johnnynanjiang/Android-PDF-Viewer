1e1dc0978efeb97c52c2778046902714caac28a9
nanjiang <johnny.nan.jiang@gmail.com>
2015-10-14 16:27:09 +1100

    [Nan Jiang] initial commit.

---
+++ app/src/androidTest/java/au/com/sping/android/pdfviewer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package au.com.sping.android.pdfviewer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
